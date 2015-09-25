a23ae65efd4f9759f9929070fe38212888d565be
mima <kukuri3@gmail.com>
2015-09-25 11:43:11 +0900

    first commit

---
+++ dataset_test/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.dataColumn3 = new System.Data.DataColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itemDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+            // 
+            // dataTable1
+            // 
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2,
+            this.dataColumn3});
+            this.dataTable1.TableName = "Table1";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "time";
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.Caption = "item";
+            this.dataColumn2.ColumnName = "item";
+            // 
+            // dataColumn3
+            // 
+            this.dataColumn3.Caption = "check";
+            this.dataColumn3.ColumnName = "check";
+            this.dataColumn3.DataType = typeof(System.Data.SqlTypes.SqlBoolean);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.timeDataGridViewTextBoxColumn,
+            this.itemDataGridViewTextBoxColumn,
+            this.checkDataGridViewTextBoxColumn});
+            this.dataGridView1.DataMember = "Table1";
+            this.dataGridView1.DataSource = this.dataSet1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(509, 340);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // timeDataGridViewTextBoxColumn
+            // 
+            this.timeDataGridViewTextBoxColumn.DataPropertyName = "time";
+            this.timeDataGridViewTextBoxColumn.HeaderText = "time";
+            this.timeDataGridViewTextBoxColumn.Name = "timeDataGridViewTextBoxColumn";
+            // 
+            // itemDataGridViewTextBoxColumn
+            // 
+            this.itemDataGridViewTextBoxColumn.DataPropertyName = "item";
+            this.itemDataGridViewTextBoxColumn.HeaderText = "item";
+            this.itemDataGridViewTextBoxColumn.Name = "itemDataGridViewTextBoxColumn";
+            // 
+            // checkDataGridViewTextBoxColumn
+            // 
+            this.checkDataGridViewTextBoxColumn.DataPropertyName = "check";
+            this.checkDataGridViewTextBoxColumn.HeaderText = "check";
+            this.checkDataGridViewTextBoxColumn.Name = "checkDataGridViewTextBoxColumn";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(53, 282);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(8, 19);
+            this.maskedTextBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(308, 388);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 17);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 417);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Data.DataSet dataSet1;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Data.DataColumn dataColumn3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn timeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itemDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn checkDataGridViewTextBoxColumn;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ dataset_test/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("winmm.dll",
+    CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        private static extern int mciSendString(string command,
+           System.Text.StringBuilder buffer, int bufferSize, IntPtr hwndCallback);
+
+        private string aliasName = "MediaFile";
+
+        public Form1()
+        {
+            InitializeComponent();
+            //dataSet1.ReadXml("data.xml");
+
+            dataGridView1.DataSource = dataSet1;
+            // 選択モードを行単位での選択のみにする
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            //DataGridView1のセルを読み取り専用にする
+            dataGridView1.ReadOnly = true;
+            //列ヘッダーを非表示にする
+            dataGridView1.ColumnHeadersVisible = false;
+            //行ヘッダーを非表示にする
+            dataGridView1.RowHeadersVisible = false;
+            //ヘッダーとすべてのセルの内容に合わせて、列の幅を自動調整する
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+            //ヘッダーとすべてのセルの内容に合わせて、行の高さを自動調整する
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataSet1.WriteXml("data.xml");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataSet1.ReadXml("data.xml");
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            // グリッドを基準としたカーソル位置のポイントを取得
+            Point p = dataGridView1.PointToClient(Cursor.Position);
+            // 取得したポイントからHitTestでセル位置取得
+            DataGridView.HitTestInfo ht = dataGridView1.HitTest(p.X, p.Y);
+            MessageBox.Show("row（行）:" + ht.RowIndex + " col（列）:" + ht.ColumnIndex);
+            int r = ht.RowIndex;
+            if(r>0)dataGridView1.Rows.RemoveAt(r);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //再生するファイル名
+            string fileName = "http://kukuri3.mydns.jp:17315/[%E3%83%8A%E3%82%B7%E3%83%A7%E3%82%B8%E3%82%AA%E3%83%81%E3%83%A3%E3%83%B3%E3%83%8D%E3%83%AB]%E3%83%A1%E3%83%BC%E3%83%87%E3%83%BC%EF%BC%81%EF%BC%93%EF%BC%9A%E8%88%AA%E7%A9%BA%E6%A9%9F%E4%BA%8B%E6%95%85%E3%81%AE%E7%9C%9F%E5%AE%9F%E3%81%A8%E7%9C%9F%E7%9B%B8%E3%80%8C%E5%BE%A1%E5%B7%A3%E9%B7%B9%E3%81%AE%E5%B0%BE%E6%A0%B9%E3%80%8D_20150921_1400_1500.mp4";
+
+            string cmd;
+            //ファイルを開く
+            cmd = "open \"" + fileName + "\" alias " + aliasName;
+            if (mciSendString(cmd, null, 0, IntPtr.Zero) != 0)
+                return;
+            //再生する
+            cmd = "play " + aliasName;
+            mciSendString(cmd, null, 0, IntPtr.Zero);
+        }
+    }
+}
